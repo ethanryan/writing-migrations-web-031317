7224e90863ae70884384731ebf690f26eb7b42fc
ethanryan <ethanryan@gmail.com>
2017-03-24 15:55:16 -0400

    done

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
 
   def change
-#    change_column :products, :part_number, :text
     change_column :students, :grade, :integer
     change_column :students, :birthdate, :datetime
   end
